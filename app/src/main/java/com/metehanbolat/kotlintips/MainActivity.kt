de9dde071c6d5aa4f6a1de6179132316cbb3cbfc
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-06 23:23:00 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/kotlintips/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.metehanbolat.kotlintips
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+@ExperimentalTime
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val (value: String, time: Duration) = measureTimedValue { someOperation() }
+        println("This operation took $time to calculate $value")
+    }
+
+    private fun someOperation(): String {
+        repeat(30_000) {
+            println('.')
+        }
+        return "finished."
+    }
+}
